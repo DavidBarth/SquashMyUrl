2f9123601031d5222b12f5c99f86fbd0acd98ed4
Dave B <Dbarthjobs@gmail.com>
2021-08-27 14:12:26 +0100

    add Url encoder and a test

---
+++ SquashMyUrlApp/SquashMyUrl.Test/UrlEncoderTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SquashMyUrlApp.Utilities;
+
+namespace SquashMyUrl.Test
+{
+    class UrlEncoderTest
+    {
+        UrlEncoder _urlEncoder;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        [TestCase("https://coinmarketcap.com/currencies/terra-luna/")]
+        public void TestUrlEncoderReturnsShortenedUrl(string inputParam)
+        {
+            _urlEncoder = new UrlEncoder(inputParam);
+
+            string encodedUrl = _urlEncoder.EncodeUrl();
+
+            Assert.True(encodedUrl.Length > 0);
+        }
+    }
+}
---
+++ SquashMyUrlApp/SquashMyUrlApp/Utilities/UrlEncoder.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace SquashMyUrlApp.Utilities
+{
+    public class UrlEncoder
+    {
+        private readonly string _inputString;
+        static long COUNTER = 100000000000;
+        string safeUrlElements = "0123456789abcdefghIjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public UrlEncoder(string inputString)
+        {
+            if(inputString != null)
+            {
+                _inputString = inputString;
+            }
+        }
+
+        public string EncodeUrl()
+        {
+            return Base10ToBase62(COUNTER);
+        }
+
+        private string Base10ToBase62(long counter)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            while(counter != 0)
+            {
+                long remainder = counter % 62;
+                int element = (int)remainder;
+                stringBuilder.Append(safeUrlElements.ElementAt(element));
+                counter /= 62;
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
